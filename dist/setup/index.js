8ab3c118d7e3039574b1ad20422b1216674bb2e0
hryk <hi@hryk.info>
2022-03-01 11:12:44 +0900

    Adds debug log

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -12235,8 +12235,10 @@ function installBlast(versionSpec) {
             "--strip",
             "1",
         ]);
+        console.log(extPath);
         core.info("Adding to the cache ...");
         const toolPath = yield tc.cacheDir(extPath, "blast", versionSpec, arch);
+        console.log(toolPath);
         core.addPath(toolPath);
         core.info("Done");
     });
